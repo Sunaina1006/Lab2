374e84b2cb1ba0fc47f0367e6f717a42e9c24078
Sunaina1006 <sunainabhola2010@gmail.com>
2024-01-25 19:58:39 -0800

    push to repo

--- Lab2/ViewController.swift
+++ Lab2/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   public  var  counter = 0
+    var Step = 1
+    @IBOutlet weak var count: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func increment(_ sender: Any) {
+        counter += Step
+        count.text = String(counter)
+    }
+    
+    @IBAction func decrement(_ sender: Any) {
+      
+        counter -= Step
+        count.text = String(counter)
+        
+    }
+    
+    @IBAction func step(_ sender: Any) {
+        Step = 2
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        Step = 1
+        counter = 0
+        count.text = String(counter)
+        
+    }
+  
 
 }
-
+ 
